5d1961c5855ef65969e518c2f7266f8ebcc9e975
mooncode163 <chyfemail163@163.com>
2020-12-03 22:20:00 +0800

    ui

--- git_upload.py
+++ git_upload.py
@@ -18,7 +18,9 @@ def upload():
     os.system("git config --global user.email \"chyfemail163@163.com\"")
     os.system("git config --global user.name \"mooncode163\"")
     os.system("git config --global user.password \"Qianlizhiwai1\"")
-     
+    
+    os.system("git branch -al") 
+    
     os.system("git add .")
     os.system("git commit -m \"ui\"")
     # os.system("git commit")  
